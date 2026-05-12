7f66b9eac06520d7d739ba142a977780ec601080
javierbarberaher <javierbarbera@iesmontsia.org>
2026-05-12 23:32:40 +0200

    Delete views directory

--- views/partials/nav.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Parcial reservat per compatibilitat amb el layout antic.
- *
- * La navegacio principal actual es views/partials/sidebar.php, inclosa des de
- * layout.php quan hi ha una sessio activa.
- */
